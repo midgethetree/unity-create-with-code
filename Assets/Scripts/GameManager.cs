6b7802de99a9e48a854dad7e4c7af0a409d2eb1a
midgethetree <76988393+midgethetree@users.noreply.github.com>
2023-06-13 04:24:44 -0400

    minor edits

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,8 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
