24471528567c1ddcd69b8d72c9be76fbd2cb74b6
midgethetree <76988393+midgethetree@users.noreply.github.com>
2023-06-13 03:25:42 -0400

    make powerups rotate

--- Assets/Scripts/PowerUp.cs
+++ Assets/Scripts/PowerUp.cs
@@ -6,5 +6,11 @@ public enum PowerUpType { None, Pushback, Rockets, Smash }
 
 public class PowerUp : MonoBehaviour
 {
+    [SerializeField] private float speed = 45.0f;
     public PowerUpType powerUpType;
+    
+    void Update()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * speed);
+    }
 }
